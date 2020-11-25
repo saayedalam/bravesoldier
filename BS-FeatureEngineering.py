ca5c9858a6c9bce677d05ecc177bab826b220e73
saayedalam <saayedalam@gmail.com>
2020-11-24 19:14:56 -0500

    daily updates

--- BS-FeatureEngineering.py
+++ BS-FeatureEngineering.py
@@ -43,108 +43,70 @@ from spacy.matcher import Matcher
 # -
 
 # Cleaning up the corpus
-def cleanup(text):
-    
-    # lowercase the corpus 
-    text = text.lower()
-    # removing apostrophes
-    text = re.sub("'s", '', str(text))
-    # removing hyphens from numbers
-    #text = re.sub('-(?<!\d)', ' ', str(text))
-    # removing punctuation
-    custom_punctuation = punctuation.replace(".", "")
-    text = text.translate(str.maketrans('', '', custom_punctuation))
-    text = re.sub("\.{2,}", '', str(text))
-    # removing emoticons
-    #text = re.sub('[^\w\s,]', ' ', str(text))
-    # removing zero-width space characters
-    text = re.sub('x200b', ' ', str(text))
-    # removing trailing whitespaces
-    text = ' '.join([token for token in text.split()])
-    # word tokenization
-    text = word_tokenize(text)
-    # additional removal of unnecessary words
-    stopwords_extra = ['im', 'ive', 'dont', 'didnt', 'doesnt', 'isnt', 
-                       'couldnt', 'na', 'youre', 'cant', 'u', 'id', 'wasnt', 
-                       'gon', 'pas', 'ill', 'youve', 'wont', 'havent', 
-                       'wouldnt', '10184285', '179180', 'arent', 'youll', 'as', 
-                       'oh', 'wan', 'av', 'p', 'ta', '10000', '6000']
-    text = [word for word in text if not word in stopwords_extra]
-    # join the words
-    text = ' '.join(text)
-
+def cleanup(text):    
+    text = text.lower() # lowers the corpus
+    text = re.sub("n't", '', str(text)) # removes not with apostrophe
+    text = re.sub("'\w+|’\w+", '', str(text)) # removes contraction
+    text = re.sub('-(?<!\d)', ' ', str(text)) # removing hyphens from numbers
+    custom_punctuation = punctuation.replace(".", "") # create custom punctuation list
+    text = text.translate(str.maketrans('', '', custom_punctuation)) # removes all punctuation except period
+    text = re.sub("\.{2,}", '', str(text)) # removes "..."
+    text = ' '.join([token for token in text.split()]) # removes trailing whitespaces
+    text = word_tokenize(text) # tokenize words
+    stopwords_extra = ['na', 'u', 'id', 'gon', 'pas', '10184285', '179180', 'as', 'oh', 'wan', 'av', 'p', 'ta', '10000', '6000']
+    text = [word for word in text if not word in stopwords_extra] # remove custome stopwords
+    text = ' '.join(text) # join the words back together  
     return text
 
 
-# Cleaning up the corpus
-def rulebased(text):
-    text = sent_tokenize(text)
-   
-    return text
-
-
+# loading the corpus data
 df = pd.read_csv('rleaves.csv', encoding='utf-8')
 
 # apply preprocessing function
-df = pd.DataFrame(df['raw'].apply(cleanup))
+corpus = pd.DataFrame(df['raw'].apply(cleanup))
 
-df = pd.DataFrame(df['raw'].apply(rulebased))
-
-df['has_numbers'] = pd.np.where(df.raw.str.contains('\d+'), True, False)
-
-df['has_time'] = pd.np.where(df.raw.str.contains('\s*days* |\s*months* |\s*weeks* |\s*ye?a?rs* '), True, False)
-
-rleaves = df.loc[(df['has_time'] == True) & (df['has_numbers'] == True)].reset_index(drop=True)
-
-# + jupyter={"source_hidden": true}
-#nlp = spacy.load("en_core_web_sm")
-#matcher = Matcher(nlp.vocab)
-#
-#doc = nlp(rleaves.raw[0])
-#
-#pattern = [{'DEP': 'acl'}, {'POS': 'NOUN'}]
-#matcher.add("numeric modifier", None, pattern)
-#matches = matcher(doc)
-#for match_id, start, end in matches:
-#    # Get the matched span
-#    matched_span = doc[start:end]
-#    print(matched_span.text)
-#
-#def set_sentiment(matcher, doc, i, matches):
-#    doc.sentiment += 0.1
-#
-#pattern1 = [{"ORTH": "Google"}, {"ORTH": "I"}, {"ORTH": "/"}, {"ORTH": "O"}]
-#pattern2 = [[{"ORTH": emoji, "OP": "+"}] for emoji in ["😀", "😂", "🤣", "😍"]]
-#matcher.add("GoogleIO", None, pattern1)  # Match "Google I/O" or "Google i/o"
-#matcher.add("HAPPY", set_sentiment, *pattern2)  # Match one or more happy emoji
-#
-#doc = nlp("A text about Google I/O 😀😀")
-#matches = matcher(doc)
-#
-#for match_id, start, end in matches:
-#    string_id = nlp.vocab.strings[match_id]
-#    span = doc[start:end]
-#    print(string_id, span.text)
-#print("Sentiment", doc.sentiment)
-# -
-
-sentokenized = rleaves['raw'].str.split('.').apply(pd.Series, 1).stack()
-
+# create a new series separated by rows by sentences
+sentokenized = corpus['raw'].str.split('.').apply(pd.Series, 1).stack()
 sentokenized.index = sentokenized.index.droplevel(-1)
-
 sentokenized.name = 'raw'
+sentokenized = pd.DataFrame(sentokenized)
 
-del rleaves['raw']
+# selecting only the rows with time value
+sentokenized['num'] = pd.np.where(sentokenized.raw.str.contains('\d+'), True, False)
+sentokenized['time'] = pd.np.where(sentokenized.raw.str.contains('\s*days* |\s*months* |\s*weeks* |\s*ye?a?rs* '), True, False)
+rleaves = sentokenized.loc[(sentokenized['num'] == True) & (sentokenized['time'] == True)].reset_index().rename(columns={'index': "post"})
 
-rleaves = rleaves.join(sentokenized)
+rleaves
 
-rleaves.shape
+nlp = spacy.load('en_core_web_sm')
 
-rleaves['has_numbers1'] = pd.np.where(rleaves.raw.str.contains('\d+'), True, False)
-rleaves['has_time1'] = pd.np.where(rleaves.raw.str.contains('\s*days* |\s*months* |\s*weeks* |\s*ye?a?rs* '), True, False)
+matcher = Matcher(nlp.vocab)
 
-new_rleaves = rleaves.loc[(rleaves['has_time1'] == True) & (rleaves['has_numbers1'] == True)].reset_index(drop=True).drop(columns=['has_time', 'has_numbers'])
+doc = nlp(' '.join(rleaves.raw))
 
-new_rleaves
+spacy.explain('acomp')
 
-# !jupytext --to py BS-TextExtraction.ipynb
+rleaves.raw[666]
+
+pattern = [{'DEP': 'acomp'}]
+matcher.add("test", None, pattern)
+matches = matcher(doc)
+
+for match_id, start, end in matches:
+    # Get the matched span
+    matched_span = doc[start:end]
+    print(matched_span.text)
+
+
+def get_matches(text):
+    doc = nlp(text)
+    pattern = [{'DEP': 'acomp'}]
+    matcher.add("test", None, pattern)
+    matches = matcher(doc)
+    for match_id, start, end in matches:
+        matched_span = doc[start:end]
+        print(matched_span.text)    
+
+
+
+# !jupytext --to py BS-FeatureEngineering.ipynb
