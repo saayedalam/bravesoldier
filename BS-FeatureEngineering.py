657ea1713bc419f6895508e7563b18a666f6f600
saayedalam <saayedalam@gmail.com>
2020-11-26 18:50:16 -0500

    daily updates

--- BS-FeatureEngineering.py
+++ BS-FeatureEngineering.py
@@ -75,24 +75,72 @@ sentokenized = pd.DataFrame(sentokenized)
 sentokenized['num'] = pd.np.where(sentokenized.raw.str.contains('\d+'), True, False)
 sentokenized['time'] = pd.np.where(sentokenized.raw.str.contains('\s*days* |\s*months* |\s*weeks* |\s*ye?a?rs* '), True, False)
 rleaves = sentokenized.loc[(sentokenized['num'] == True) & (sentokenized['time'] == True)].reset_index().rename(columns={'index': "post"})
+rleaves['totalwords'] = rleaves['raw'].str.split().str.len()
+rleaves['day'] = pd.np.where(rleaves.raw.str.contains('\d+\s*day[s\s]|\s*day\s*\d+'), True, False)
+rleaves['week'] = pd.np.where(rleaves.raw.str.contains('\d+\s*week[s\s]|\s*week\s*\d+'), True, False)
+rleaves['month'] = pd.np.where(rleaves.raw.str.contains('\d+\s*month[s\s]|\s*month\s*\d+'), True, False)
+rleaves['year'] = pd.np.where(rleaves.raw.str.contains('\d+\s*ye?a?r[s\s]*'), True, False)
 
-rleaves
+no_dates = rleaves.loc[(rleaves['day'] == False) & (rleaves['week'] == False) & (rleaves['month'] == False) & (rleaves['year'] == False)].reset_index(drop=True)
 
 nlp = spacy.load('en_core_web_sm')
+doc = nlp('we now get over 145000 unique users a month here at rleaves so even a small participation can help a whole lot of people')
+
+for ent in doc.ents:
+    print(ent.text, ent.label_)
+
+# +
+import spacy
+from spacy import displacy
+
+nlp = spacy.load("en_core_web_sm")
+text = 'you also realise you are about £100 better off every single week which is the equivalent of getting a 5 grand pay rise at work tell me that isn a good incentive eh here the secret the difficult part of quitting is getting yourself to a place mentally where you are 100 ready to leave weed behind and move on with your life'
+doc = nlp(text)
+displacy.render(doc, style='ent')
+
+# + jupyter={"outputs_hidden": true}
+str(rleaves.raw.tolist())
+
+# +
+import spacy
+from spacy.matcher import Matcher
+
+nlp = spacy.load("en_core_web_sm")
+
+text = str(rleaves.raw.tolist())
+doc = nlp(text)
 
 matcher = Matcher(nlp.vocab)
+pattern = [{'LIKE_NUM': True}, {'LOWER': {'REGEX' : 'day'}}]
+matcher.add('TIME', None, pattern)
 
-spacy.explain('dobj')
+matches = matcher(doc)
+print([doc[start:end] for match_id, start, end in matches])
+#=> [5am, 6am, 9pm, 6am, 6am, 9pm]
+# -
 
-np.random.random_integers(1309)
+def validate(text):
+    nlp = spacy.load('en_core_web_sm')
+    docs = list(nlp.pipe(text))
+    entities = [doc.ents for doc in docs]
+    options = {"ents": ['DATE']}
+    displacy.render(docs, style="ent", options=options)
 
-rleaves.shape
 
-rleaves.raw[np.random.random_integers(1309)]
+rleaves.raw
+
+validate(rleaves.raw)
+
+no_dates.raw.values.tolist()
+
+# load english language model
+nlp = spacy.load('en_core_web_sm')
+matcher = Matcher(nlp.vocab)
 
 
-def extract_acomp_dep(text):
-    pattern = [{'ORTH': 'mood'}]
+def find_days(text):
+    pattern = [{'DEP': 'pobj'},
+               {'POS': 'NOUN'}]
     #pattern = [{'DEP': 'ccomp'}]
     matcher.add("test", None, pattern)
     matches = matcher(text)
@@ -102,9 +150,11 @@ def extract_acomp_dep(text):
         print(span.text)    
 
 
-doc = nlp(' '.join(rleaves.raw))
+find_days(doc)
 
-extract_acomp_dep(doc)
+rleaves.raw[541]
+
+doc = nlp(' '.join(rleaves.raw))
 
 # !jupytext --to py BS-FeatureEngineering.ipynb
 
