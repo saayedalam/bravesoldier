aa31519c1bf885e961f820bdfb6030d14171a604
saayedalam <saayedalam@gmail.com>
2021-01-11 18:43:34 -0500

    new updates

--- src/BS-TextExtraction.py
+++ src/BS-TextExtraction.py
@@ -5,18 +5,18 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.6.0
+#       jupytext_version: 1.7.1
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
 
-# +
 # Library for Reddit API
 import praw
 import pandas as pd
 
+# +
 # Reddit API Credentials
 reddit = praw.Reddit(client_id='7_PY9asBHeVJxw',
                      client_secret='KL01wgTYZqwEDdPH-R8vNBqFYe4',
@@ -26,27 +26,42 @@ reddit = praw.Reddit(client_id='7_PY9asBHeVJxw',
 
 # Pull the subreddit of 
 subreddit = reddit.subreddit('leaves')
+# -
 
 # Pulling top 1000 posts of leaves subreddit
 leaves_subreddit = reddit.subreddit('leaves').top(limit=1000)
 
+# +
 # Create an empty dictionary to save data
 dict = {'title': [],
         'body': [],
+        'author': [],
+        'time': [],
+        'text_only': [],
+        'num_comments': [],
        }
 
 # Storing the data in the empty dictionary
-for submission in leaves_subreddit:
-    dict['title'].append(submission.title)
-    dict['body'].append(submission.selftext)
-
+for post in leaves_subreddit:
+    dict['title'].append(post.title)
+    dict['body'].append(post.selftext)
+    dict['author'].append(post.author)
+    dict['time'].append(post.created_utc)
+    dict['text_only'].append(post.is_self)
+    dict['num_comments'].append(post.num_comments)
+    
 # Convert the data to pandas dataframe and apply date function
 df = pd.DataFrame(dict)
+# -
+
 df['raw'] = df['title'] + ' ' + df['body']
 df.drop(['title', 'body'], axis=1, inplace=True)
 
+df
+
 # Save it as CSV
-df.to_csv('rleaves.csv', index=False)
-# -
+df.to_csv('input\rleaves.csv', index=False)
+
+# !ls
 
 # !jupytext --to py BS-TextExtraction.ipynb
