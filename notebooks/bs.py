50e29410040b52bd932c7a7e1f0b47ae712c95a5
saayedalam <saayedalam@gmail.com>
2021-01-21 19:14:12 -0500

    new updates

--- notebooks/bs.py
+++ notebooks/bs.py
@@ -1,4 +1,5 @@
 import urllib
+import pandas as pd
 import streamlit as st
 from PIL import Image
 
@@ -12,15 +13,41 @@ def main():
     
     if sidebar == "Introduction":
         introduction()
-    
+    elif sidebar == "Post":
+        posts()
 
 def introduction():
     intro = st.beta_expander('Introduction', expanded=True)
-    intro.write(get_file_content_as_string("introduction.md"))
+    intro.markdown(get_file_content_as_string("introduction.md"))
     image = Image.open('wordcloud_user_leaves.png')
     st.image(image, caption='Word Cloud of r/leaves', use_column_width=True)
+    with st.beta_expander("Code"):
+        st.code('x=x', language='python')
+        
+def posts():
+    df1 = pd.read_csv('rleaves.csv', encoding='utf-8')
+    df1 = df1[['raw', 'time']]
+    df2 = pd.read_csv('rleaves_clean.csv', encoding='utf-8')
+    col1, col2 = st.beta_columns(2)
+    col1.subheader('Before Cleanup')
+    col1.dataframe(df1)
+    col2.subheader('After Cleanup')
+    col2.dataframe(df2)
+    with st.beta_expander("Code"):
+        st.code(get_file_content_as_string("text_cleanup.py"), language='python')
     
-#@st.cache(show_spinner=False)
+    # Most common words
+    st.subheader("Most Used Words of r/leaves")
+    #number = st.number_input('Select a number to show count', max_value=85229, value=50) # streamlit
+    #top_words = Counter(' '.join(rleaves['raw']).split()).most_common(int(number))
+    #top_words = pd.DataFrame(top_words, columns=['word', 'count'])
+    #fig = px.bar(top_words, x='count', y='word', orientation='h', template='plotly_white')
+    #fig.update_yaxes(visible=False, categoryorder='total ascending')
+    #fig.update_layout(hovermode="x unified")
+    #fig.update_traces(marker_color='green')
+    #st.plotly_chart(fig, use_container_width=True)
+    
+@st.cache(show_spinner=False)
 def get_file_content_as_string(path):
     url = 'https://raw.githubusercontent.com/saayedalam/bravesoldier/main/notebooks/' + path
     response = urllib.request.urlopen(url)
